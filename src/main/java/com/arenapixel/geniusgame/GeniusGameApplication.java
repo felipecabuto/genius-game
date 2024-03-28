a12fec174106512ee97feb660bb810c90cf24dac
Felipe Cabuto <felipecabuto@gmail.com>
2024-03-28 19:55:46 -0300

    Inicia projeto Spring Boot para jogo Genius

---
+++ src/main/java/com/arenapixel/geniusgame/GeniusGameApplication.java
@@ -0,0 +1,13 @@
+package com.arenapixel.geniusgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniusGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arenapixel/geniusgame/GeniusGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenapixel.geniusgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
