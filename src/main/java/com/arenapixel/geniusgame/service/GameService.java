d0c33a15473e2914c4f91b5619d7c0ac11f5d949
Felipe Cabuto <felipecabuto@gmail.com>
2024-03-28 20:54:38 -0300

    criando a service

---
+++ src/main/java/com/arenapixel/geniusgame/service/GameService.java
@@ -0,0 +1,27 @@
+package com.arenapixel.geniusgame.service;
+
+import com.arenapixel.geniusgame.model.Game;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    private Game game;
+
+    public void startNewGame() {
+        game = new Game();
+    }
+
+    public boolean play(List<String> playerSequence) {
+        return game.checkSequence(playerSequence);
+    }
+
+    public int getScore() {
+        return game.getScore();
+    }
+
+    public boolean isGameOver() {
+        return !game.isActive();
+    }
+}
